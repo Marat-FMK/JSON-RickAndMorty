7089007981303204e69b076659611951fdcb6fe6
MaratXcode <kosjachok8@yandex.ru>
2024-07-17 15:39:22 +0300

    work with model and storybord

---
+++ ParsJSON/ViewControllers/Model/RickAndMorty.swift
@@ -0,0 +1,74 @@
+//
+//  RickAndMorty.swift
+//  ParsJSON
+//
+//  Created by Marat Fakhrizhanov on 17.07.2024.
+//
+
+
+
+
+struct RickAndMorty: Decodable {
+    let info: Info
+    let resault: [Character]
+}
+
+struct Info: Decodable {
+    let count: Int
+    let page : Int
+    let next: String?
+    let prev: String?
+}
+
+struct Character: Decodable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: Location
+    let location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    
+    var description: String {
+        """
+Name: \(name)
+Statys: \(status)
+Species: \(species)
+Gender: \(gender)
+Origin: \(origin.name)
+Location: \(location.name)
+"""
+    }
+}
+
+struct Location: Decodable {
+    let name: String
+}
+
+struct Episode: Decodable {
+    let name: String
+    let data: String
+    let episode: String
+    let characters: [String]
+    
+    var description: String {
+        """
+Title: \(name)
+Data: \(data)
+"""
+    }
+        enum CodingKeys: String, CodingKey {
+            case name = "name"
+            case data = "air_date"
+            case episode = "episode"
+            case characters = "characers"
+        }
+    }
+
+enum Link: String {
+    case rickAdnMortyApi = "https://rickandmortyapi.com/api/character"
+}
+
